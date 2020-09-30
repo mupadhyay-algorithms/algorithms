4ddcad4b26ade2fffb5fae74ef06af284f352b37
Mahesh Upadhyay <71276418+mupadhyay-algorithms@users.noreply.github.com>
2020-09-30 16:37:08 -0700

    Create Solution.cpp

---
+++ HackerRank-Plusminus/Solution.cpp
@@ -0,0 +1,135 @@
+/*
+
+Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line with  places after the decimal.
+
+Note: This challenge introduces precision problems. The test cases are scaled to six decimal places, though answers with absolute error of up to  are acceptable.
+
+Example
+
+There are  elements, two positive, two negative and one zero. Their ratios are ,  and . Results are printed as:
+
+0.400000
+0.400000
+0.200000
+Function Description
+
+Complete the plusMinus function in the editor below.
+
+plusMinus has the following parameter(s):
+
+int arr[n]: an array of integers
+Print
+Print the ratios of positive, negative and zero values in the array. Each value should be printed on a separate line with  digits after the decimal. The function should not return a value.
+
+Input Format
+
+The first line contains an integer, , the size of the array.
+The second line contains  space-separated integers that describe .
+
+Constraints
+
+
+
+Output Format
+
+Print the following  lines, each to  decimals:
+
+proportion of positive values
+proportion of negative values
+proportion of zeros
+Sample Input
+
+6
+-4 3 -9 0 4 1         
+Sample Output
+
+0.500000
+0.333333
+0.166667
+Explanation
+
+There are  positive numbers,  negative numbers, and  zero in the array.
+The proportions of occurrence are positive: , negative:  and zeros: .
+
+*/
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<string> split_string(string);
+#define NEGATIVE 0
+#define ZERO 1
+#define POSITIVE 2
+
+// Complete the plusMinus function below.
+void plusMinus(vector<int> arr) {
+    int size=arr.size();
+    vector<int> count(3,0);
+    for (int i =0; i <size; i++)
+    {
+        if (arr[i]<0)
+            count[NEGATIVE]++;
+        else if (arr[i]==0)
+            count[ZERO]++;
+        else if (arr[i]>0)
+            count[POSITIVE]++;
+    }
+    cout << ((double)count[POSITIVE])/(double)size <<endl;
+    cout << ((double)count[NEGATIVE])/(double)size <<endl;
+    cout << ((double)count[ZERO])/(double)size <<endl;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split_string(arr_temp_temp);
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    plusMinus(arr);
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
+
